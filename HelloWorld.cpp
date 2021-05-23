f548276633fd33a057cb3e74606891d046b3444a
GithubBoy3075 <78290908+GithubBoy3075@users.noreply.github.com>
2021-05-23 09:58:22 +0000

    Create HelloWorld.cpp

---
+++ HelloWorld.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+#include<windows.h>
+// or include ".\GitHub.h"   (Download from the "https://githubuser1024.GitHub.io/GitHub.h" page)
+using namespace std;
+int main(){
+    cout<<"Hello,World!"<<endl;
+    system("pause");
+    return 0;
+}
